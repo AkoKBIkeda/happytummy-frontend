8b622a5da01dbffd90eae371952f8ff391f2d550
AkoKBIkeda <akikoi1024@gmail.com>
2025-08-13 03:31:34 +1200

    made the logo clickable and take the user to the homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from '../assets/kAI.png'; // adjust path if needed
+import logo from '../assets/kAI.png';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="flex items-center p-10 select-none cursor-default">
-      <img src={logo} alt="kAI Logo" className="h-12 w-auto" />
+        <Link to="/">
+            <img src={logo} alt="kAI Logo" className="h-12 w-auto" />
+        </Link>
     </header>
   );
 }
